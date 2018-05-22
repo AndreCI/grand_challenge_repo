d1fe96b6353d106e92dacc50a77f3af68fbda7c5
AndreCI <andre.cibils@gmail.com>
2018-05-22 08:19:57 +0200

    added util python scripts.

---
+++ run/extract_write.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import os
+import csv
+
+def extract_results(current_dir):
+ results = (current_dir + '/' + 'BestAvgStd.txt')
+ data = {}
+ with open(results, 'r', encoding='utf-8') as f:
+  last_line = (f.readlines()[-1])[:-1].split(' ') #remove \n
+ data['generations_done']=last_line[0]
+ data['best_score']=last_line[1]
+ data['avg_score']=last_line[2]
+ data['std_score']=last_line[3]
+ return data
+ 
+def extract_evol_conf(file):
+ conf_path = file.split('/')
+ conf_path = conf_path[0] + '/' + conf_path[1] + '/'
+ with open(file) as f:
+  lines = (f.readlines())
+ data = {}
+ for l in lines:
+  if l[0] != '#':
+   l = l[:-1]
+   current = l.split('=')
+   if current[0] == 'simulatorConfFile': #get path for conf_file
+    conf_path += current[1]
+   data[current[0]] = current[1] #add key (current[0]) and val (current[1]) to dict
+ additional_data = extract_conf(conf_path)
+ for d in additional_data:
+  data[d[0]] = d[1]
+ return data
+
+def extract_conf(file):
+ with open(file) as f:
+  lines = f.readlines()
+ data = []
+ for l in lines:
+  if l[0] != '#': #remove comments
+   l = l[:-1] #remove \n
+   current = l.split('=')
+   data.append(current)
+ return data
+ 
+def write_into_csv(data, file):
+ print(data)
+ exist = True
+ if not os.path.exists(file): #check if we need the header
+  exist = False
+ with open(file, 'w', encoding='utf-8') as f:
+  csvwriter = csv.DictWriter(f, data.keys())
+  if not exist:
+   csvwriter.writeheader()
+  csvwriter.writerow(data)
+ 
---
+++ run/launch_evo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import os
+import sys
+import subprocess
+import shlex
+import argparse
+from extract_write import extract_evol_conf, write_into_csv, extract_results
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--conf_number', help='Number of the evol Conf file', default='1', type=str)
+parser.add_argument('--seed', help='Seed for the evo', default=1, type=int)
+parser.add_argument('--wriring_path', help='Where to write the exp', default='../outputs/current', type=str)
+parser.add_argument('--csv_name', help='Csv file to write results', default='../results.csv', type=str)
+opt = parser.parse_args()
+
+run_path = "C:\\Users\\andre\\Documents\\robogen-windows\\run\\"
+seed = str(opt.seed)
+writing_dir = opt.wriring_path
+evol_conf = '../data/EvolConf_' + str(opt.conf_number) + '.txt'
+
+metadata = extract_evol_conf(evol_conf)
+subprocess.Popen([run_path + "robogen-server.exe", '8001'])
+subprocess.call([run_path + "robogen-evolver.exe", seed, writing_dir, evol_conf, '--overwrite'])
+
+results = extract_results(writing_dir)
+to_write = {**metadata, **results}
+write_into_csv(to_write, opt.csv_name)
